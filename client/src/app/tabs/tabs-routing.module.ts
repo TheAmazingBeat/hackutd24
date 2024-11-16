c446d1468463bca1c601a2578b0810a725672979
Ice <ice.baldivas@gmail.com>
2024-11-16 17:29:28 -0600

    initialize routes

--- client/src/app/tabs/tabs-routing.module.ts
+++ client/src/app/tabs/tabs-routing.module.ts
@@ -4,33 +4,36 @@ import { TabsPage } from './tabs.page';
 
 const routes: Routes = [
   {
-    path: 'tabs',
+    path: '',
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        path: 'for-you',
+        loadChildren: () =>
+          import('../tab1-for-you/tab1.module').then((m) => m.Tab1PageModule),
       },
       {
-        path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+        path: 'accounts',
+        loadChildren: () =>
+          import('../tab2-accounts/tab2.module').then((m) => m.Tab2PageModule),
       },
       {
-        path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+        path: 'profile',
+        loadChildren: () =>
+          import('../tab3-profile/tab3.module').then((m) => m.Tab3PageModule),
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
-        pathMatch: 'full'
-      }
-    ]
+        redirectTo: '/for-you',
+        pathMatch: 'full',
+      },
+    ],
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
-    pathMatch: 'full'
-  }
+    redirectTo: '/for-you',
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
