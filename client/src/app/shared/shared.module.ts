44f8c5c79fc9e6567bf4ee4e0ddaabb409164852
Ice <ice.baldivas@gmail.com>
2024-11-17 01:32:07 -0600

    fix for you

---
+++ client/src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+
+@NgModule({
+  imports: [CommonModule, IonicModule],
+  exports: [HeaderComponent],
+  declarations: [HeaderComponent],
+  providers: [],
+})
+export class NameModule {}
