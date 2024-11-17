bec92f0af8dd367c918ce62e277a19f38d8e093c
Leon <leon.zh113@gmail.com>
2024-11-17 09:49:05 -0600

    bakcned

--- backend/routes/fyp.py
+++ backend/routes/fyp.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from routes.api import get_company_investment_data_and_graph
+import pandas as pd
 import json
 import csv
 import os
@@ -7,8 +8,10 @@ import random
 
 fyp_bp = Blueprint("fyp", __name__)
 curr_dir = os.getcwd()
-stock_data_fp = os.path.join(curr_dir, 'userDocs', 'stocks_data_with_scores.csv')
-user_data_fp = os.path.join(curr_dir, 'userDocs', 'stocks_data_with_scores.csv')
+print(curr_dir)
+result_fp = os.path.join(curr_dir, 'model', 'result.csv')
+user_data_fp = os.path.join(curr_dir, 'userDocs', 'userData.json')
+stock_data_fp = os.path.join(curr_dir, 'model', 'stocks_data.csv')
 images = []
 companies = []
 currentComapnies = []
@@ -22,6 +25,11 @@ def for_you():
 # when user clicks refresh button
 @fyp_bp.route("/fyp/refresh")
 def refresh_Page():
+    df = pd.read_csv(stock_data_fp)
+    for company in currentComapnies:
+        df.loc[df['Company Name'] == company, 'User Decision'] == "No"
+    df.to_csv(stock_data_fp, index = False)
+
     init_page()
     return
 
@@ -30,13 +38,20 @@ def refresh_Page():
 def investment_popup():
     return
 
-# When user clicks yes to invest
 @fyp_bp.route("/fyp/investment/yes")
+def user_click_yes(company):
+    df = pd.read_csv(stock_data_fp)
+    df.loc[df['Company Name'] == company, 'User Decision'] == "Yes"
+    df.to_csv(stock_data_fp, index = False)
+
+# When user clicks confirm investment amount
+@fyp_bp.route("/fyp/investment/yes/confirm")
 def enter_investment_amount(company, stock_name, investment_amount, num_of_stocks):
     user_data = load_json()
     user_data["Account Balance"] = user_data["Account Balance"] - investment_amount
     user_data["Investments"].append((company, stock_name, num_of_stocks))
     json.dump(user_data, user_data_fp)
+
 
 # When user presses save button
 @fyp_bp.route("/fyp/investments/save")
@@ -44,30 +59,44 @@ def save_investment(company):
     user_data = load_json()
     user_data["Saved Investments"].append(company)
     json.dump(user_data, user_data_fp)
+    df = pd.read_csv(stock_data_fp)
+    df.loc[df['Company Name'] == company, 'User Decision'] == "Maybe"
+    df.to_csv(stock_data_fp, index = False)
+
 
 # When user presses no on investment page
 @fyp_bp.route("/fyp/investment/no")
 def select_no(company):
     update_Current_Companies(company)
-    return
+    df = pd.read_csv(stock_data_fp)
+    df.loc[df['Company Name'] == company, 'User Decision'] == "No"
+    df.to_csv(stock_data_fp, index = False)
 
 def update_Current_Companies(company):
     global currentComapnies
     currentComapnies.remove(company)
 
 def get_Top_Investments():  
+    excluded_companies = set()
+    with open(stock_data_fp, mode='r') as stock_file:
+        stock_reader = csv.DictReader(stock_file)
+        for row in stock_reader:
+            if row["User Decision"] == "No":
+                excluded_companies.add(row["Company Name"])
+
     company_scores = []
-    with open(stock_data_fp, mode = 'r') as file:
+    with open(result_fp, mode = 'r') as file:
         csvFile = csv.reader(file)
         next(csvFile,None)
 
         for row in csvFile:
             company = row[0]
             stock_name = row[1]  
-            score = row[6]  
-            company_scores.append((company, stock_name, score))
+            score = row[2]  
+            
+            if company not in excluded_companies: 
+                company_scores.append((company, stock_name, score))
 
-    company_scores.sort(key=lambda x: x[2], reverse=True)   
     print(company_scores)
     return company_scores
 
